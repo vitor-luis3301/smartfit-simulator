744c5785d95dc76f4fe9d7a5d72ee8a21466db09
vitor-luis3301 <superpowervitor2@gmail.com>
2024-08-20 11:22:23 -0300

    Added treadmills in tilemap

--- map.gd
+++ map.gd
@@ -1,23 +1,29 @@
-extends TileMap
+extends Node2D
 
-@export var GridSize = 16
+@export var GridSizeX = 33
+@export var GridSizeY = 20
 @export var Dic = {}
 
 func _ready() -> void:
-	for x in GridSize:
-		for y in GridSize:
+	for x in GridSizeX:
+		for y in GridSizeY:
 			Dic[str(Vector2(x,y))] = {
 				"Type" : "Floor"
 			}
-			set_cell(0, Vector2(x, y), 0, Vector2i(0, 0), 0)
+			$Layer0.set_cell(Vector2(x, y), 0, Vector2i(0, 0), 0)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var tile = local_to_map(get_global_mouse_position())
+	var tile = $Layer0.local_to_map(get_global_mouse_position())
 	
-	for x in GridSize:
-		for y in GridSize:
-			erase_cell(1, Vector2(x,y))
+	for x in GridSizeX:
+		for y in GridSizeY:
+			$Layer1.erase_cell(Vector2(x,y))
 	
 	if Dic.has(str(tile)):
-		set_cell(1, tile, 1, Vector2i(0,0), 0)
+		$Layer1.set_cell(tile, 1, Vector2i(0,0), 0)
+	
+	if Input.is_action_pressed("left_button"):
+		$Layer2.set_cell(tile, 2, Vector2i(0,0), 0)
+	if Input.is_action_pressed("right_button"):
+		$Layer2.erase_cell(tile)
