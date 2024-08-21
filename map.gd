ee7165f6a498e341097c13b96c91f61c8bd806b1
vitor-luis3301 <superpowervitor2@gmail.com>
2024-08-21 09:20:21 -0300

    Added more sprites and hability to select items

--- map.gd
+++ map.gd
@@ -4,6 +4,14 @@ extends Node2D
 @export var GridSizeY = 20
 @export var Dic = {}
 
+var selectedItem = 0
+
+var items = [
+	"threadmill",
+	"med_weight",
+	"big_weight",
+]
+
 func _ready() -> void:
 	for x in GridSizeX:
 		for y in GridSizeY:
@@ -23,7 +31,19 @@ func _process(delta: float) -> void:
 	if Dic.has(str(tile)):
 		$Layer1.set_cell(tile, 1, Vector2i(0,0), 0)
 	
+	if Input.is_action_just_pressed("mouse_wheel_down"):
+		selectedItem += 1
+	if Input.is_action_just_pressed("mouse_wheel_up"):
+		selectedItem -= 1
+	if selectedItem < 0:
+		selectedItem = 0
+	
 	if Input.is_action_pressed("left_button"):
-		$Layer2.set_cell(tile, 0, Vector2i(0,0), 0)
+		$Layer2.set_cell(tile, selectedItem, Vector2i(0,0), 0)
 	if Input.is_action_pressed("right_button"):
 		$Layer2.erase_cell(tile)
+	
+	if selectedItem+1 > items.size():
+		%selectedItem.text = "null"
+	else:
+		%selectedItem.text = "[rainbow][wave]" + "Item: " + items[selectedItem] + "[/wave][/rainbow]"
