1c2381e9a60343c78826320fb84884863988ba53
vitor-luis3301 <superpowervitor2@gmail.com>
2024-08-22 11:41:23 -0300

    Update 2.0

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -7,9 +7,9 @@ extends Node2D
 var selectedItem = 0
 
 var items = [
-	"threadmill",
 	"med_weight",
 	"big_weight",
+	"threadmill"
 ]
 
 func _ready() -> void:
@@ -18,30 +18,45 @@ func _ready() -> void:
 			Dic[str(Vector2(x,y))] = {
 				"Type" : "Floor"
 			}
-			$Layer0.set_cell(Vector2(x, y), 0, Vector2i(0, 0), 0)
+			$'Layer-1'.set_cell(Vector2(x, y), 0, Vector2i(0, 0), 0)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var tile = $Layer0.local_to_map(get_global_mouse_position())
+	var mouse = get_global_mouse_position()
+	
+	var tile = $Layer0.local_to_map(mouse)
+	$Layer2.clear()
+	if selectedItem >= 2:
+		tile = $Layer3.local_to_map(mouse)
+		$Layer0.clear()
 	
 	for x in GridSizeX:
 		for y in GridSizeY:
-			$Layer1.erase_cell(Vector2(x,y))
+			$Layer0.erase_cell(Vector2(x,y))
+			if selectedItem >= 2:
+				$Layer2.erase_cell(Vector2(x,y))
 	
 	if Dic.has(str(tile)):
-		$Layer1.set_cell(tile, 1, Vector2i(0,0), 0)
-	
-	if Input.is_action_just_pressed("mouse_wheel_down"):
+		$Layer0.set_cell(tile, 1, Vector2i(0,0), 0)
+		if selectedItem >= 2:
+			$Layer2.set_cell(tile, 0, Vector2i(0,0), 0)
+			
+	if Input.is_action_just_pressed("select_down"):
 		selectedItem += 1
-	if Input.is_action_just_pressed("mouse_wheel_up"):
+	if Input.is_action_just_pressed("select_up"):
 		selectedItem -= 1
 	if selectedItem < 0:
 		selectedItem = 0
 	
 	if Input.is_action_pressed("left_button"):
-		$Layer2.set_cell(tile, selectedItem, Vector2i(0,0), 0)
+		$Layer1.set_cell(tile, selectedItem, Vector2i(0,0), 0)
+		if selectedItem >= 2:
+			$Layer3.set_cell(tile, selectedItem-2, Vector2i(0,0), 0)
 	if Input.is_action_pressed("right_button"):
-		$Layer2.erase_cell(tile)
+		$Layer1.erase_cell(tile)
+		if selectedItem >= 2:
+			$Layer3.erase_cell(tile)
+		
 	
 	if selectedItem+1 > items.size():
 		%selectedItem.text = "null"
