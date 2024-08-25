ef5bb44dae043c2a88b0abbf5af7fa2c00e4c059
vitor-luis3301 <superpowervitor2@gmail.com>
2024-08-25 19:54:37 -0300

    Added global variables

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -3,14 +3,17 @@ extends Node2D
 @export var GridSizeX = 33
 @export var GridSizeY = 20
 @export var Dic = {}
+@export var canClick : bool = true
 
 var spawnedEntity
 
 var selectedItem = 0
 
 var items = [
+	"small_weight",
 	"med_weight",
 	"big_weight",
+	"Giga_weight",
 	"threadmill",
 	"mat"
 ]
@@ -68,8 +71,10 @@ func _physics_process(delta: float) -> void:
 	
 	var equipPosition = tilesize * tile + Vector2i(tilesize/2, tilesize/2)
 	
-	if Input.is_action_just_pressed("left_button"):
-		currentequip.set_cell(tile, selectedItem, Vector2i(0,0), 0)
+	if Input.is_action_just_pressed("left_button") and canClick == true:
+		%smol_equip.set_cell(tile, selectedItem, Vector2i(0,0), 0)
+		if selectedItem >= 2:
+			%beeg_equip.set_cell(tile, selectedItem-2, Vector2i(0,0), 0)
 		spawnedEntity = spawn_equipmens(tilesize, items[selectedItem], equipPosition)
 	
 	# Remove equipment to the map if right mouse button is clicked
@@ -83,6 +88,12 @@ func _physics_process(delta: float) -> void:
 	else:
 		%selectedItem.text = "Item: " + items[selectedItem]
 	
+	%selectedItem.text += "\nCan you click? "
+	if canClick:
+		%selectedItem.text += "Yes, you can!"
+	else:
+		%selectedItem.text += "No, you can't."
+	
 	if Input.is_action_just_pressed("ui_cancel"):
 		get_tree().quit()
 
@@ -92,8 +103,6 @@ func spawn_equipmens(size, type, mousePos):
 	newEquip.get_node("collision").shape.size = Vector2(size, size)
 	
 	add_child(newEquip)
-	print(newEquip)
-	
 	return newEquip
 
 func delete_equipments():
@@ -102,4 +111,4 @@ func delete_equipments():
 func _on_button_pressed():
 	$NavigationRegion2D.bake_navigation_polygon()
 	await $NavigationRegion2D.bake_finished
-	$Spawner.spawn()
+	$Spawner.spawn(spawnedEntity)
