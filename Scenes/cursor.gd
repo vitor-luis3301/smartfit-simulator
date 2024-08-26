9d878827443f60041b2963700520c1d99cb5642f
vitor-luis3301 <superpowervitor2@gmail.com>
2024-08-26 00:23:13 -0300

    Added store scene. Still not implemented

--- Scenes/cursor.gd
+++ Scenes/cursor.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+@export var area : Area2D
+
 func instance_place():
 	var areas : Array = []
 	for i in get_overlapping_areas():
@@ -12,6 +14,10 @@ func instance_place():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	position = get_global_mouse_position()
+	
+	area = instance_place()
+	
+	print(area)
 	
 	if instance_place():
 		owner.canClick = false
--- Scripts/main.gd
+++ Scripts/main.gd
@@ -35,15 +35,17 @@ func _physics_process(delta: float) -> void:
 	var mouse = get_global_mouse_position()
 	# change between small tiles and big tiles
 	var currentselect = %smol_select
-	var currentequip = %smol_equip
 	
 	var tile = %smol_select.local_to_map(mouse)
 	%beeg_select.clear()
-	if selectedItem >= 2:
-		currentselect = %beeg_select
-		currentequip = %beeg_equip
-		tile = %beeg_select.local_to_map(mouse)
+	if selectedItem == 3:
+		currentselect = %mid_select
+		tile = %mid_select.local_to_map(mouse)
 		%smol_select.clear()
+	elif selectedItem >= 4:
+		currentselect = %beeg_select
+		tile = %beeg_select.local_to_map(mouse)
+		%mid_select.clear()
 	
 	# add marker for every tile that mouse is on top of
 	for x in GridSizeX:
@@ -52,9 +54,13 @@ func _physics_process(delta: float) -> void:
 	
 	# delete marker if mouse is not on tile
 	if Dic.has(str(tile)):
-		%smol_select.set_cell(tile, 1, Vector2i(0,0), 0)
-		if selectedItem >= 2:
+		%smol_select.set_cell(tile, 0, Vector2i(0,0), 0)
+		if selectedItem == 3:
 			%smol_select.clear()
+			%mid_select.set_cell(tile, 0, Vector2i(0,0), 0)
+		elif selectedItem >= 4:
+			%smol_select.clear()
+			%mid_select.clear()
 			%beeg_select.set_cell(tile, 0, Vector2i(0,0), 0)
 	
 	# Select items on mouse wheel up or down or arrow up or down is clicked
@@ -64,37 +70,37 @@ func _physics_process(delta: float) -> void:
 		selectedItem -= 1
 	if selectedItem < 0:
 		selectedItem = 0
+	if selectedItem > items.size():
+		selectedItem = items.size()
 	
 	# Add equipment to the map if left mouse button is clicked
 	var tilesize = 32 # change sample_equipments collisions' sizes to this
-	if selectedItem >= 2:
+	if selectedItem == 2:
+		tilesize = 48
+	elif selectedItem >= 3:
 		tilesize = 96
 	
+	# define the position of the equipment based on the size of the tile
 	var equipPosition = tilesize * tile + Vector2i(tilesize/2, tilesize/2)
 	
 	# Add equipment to the map when left mouse button is clicked
 	if Input.is_action_just_pressed("left_button") and canClick == true:
 		%smol_equip.set_cell(tile, selectedItem, Vector2i(0,0), 0)
-		if selectedItem >= 2:
-			%beeg_equip.set_cell(tile, selectedItem-2, Vector2i(0,0), 0)
+		if selectedItem == 3:
+			%mid_equip.set_cell(tile, 0, Vector2i(0,0), 0)
+		elif selectedItem >= 4:
+			%beeg_equip.set_cell(tile, selectedItem-4, Vector2i(0,0), 0)
 		spawnedEntity = spawn_equipmens(tilesize, items[selectedItem], equipPosition)
 	
 	# Remove equipment to the map if right mouse button is clicked
 	if Input.is_action_pressed("right_button"):
-		%smol_equip.erase_cell(tile)
-		if selectedItem >= 2:
-			%beeg_equip.erase_cell(tile)
+		pass 
+		# Implement it in another way
 	
 	if selectedItem+1 > items.size():
 		%selectedItem.text = "null"
 	else:
 		%selectedItem.text = "Item: " + items[selectedItem]
-	
-	%selectedItem.text += "\nCan you click? "
-	if canClick:
-		%selectedItem.text += "Yes, you can!"
-	else:
-		%selectedItem.text += "No, you can't."
 	
 	if Input.is_action_just_pressed("ui_cancel"):
 		get_tree().quit()
@@ -106,9 +112,6 @@ func spawn_equipmens(size, type, mousePos):
 	
 	add_child(newEquip)
 	return newEquip
-
-func delete_equipments():
-	pass
 
 func _on_button_pressed():
 	$NavigationRegion2D.bake_navigation_polygon()
